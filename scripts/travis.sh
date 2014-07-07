1b878b7ff1f003e558baafe1b58ddea10d8c060d
Alec Thomas <alec@swapoff.org>
2014-07-07 14:09:32 +1000

    Maybe fix the build?

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -1,15 +1,5 @@
 #!/bin/bash -e
 
-CMAKE_ARGS="-DCMAKE_BUILD_TYPE=Debug -DENTITYX_BUILD_TESTING=1"
-
-if [ "$USE_STD_SHARED_PTR" = "1" ]; then
-  CMAKE_ARGS="${CMAKE_ARGS}"
-  # This fails on OSX
-  if [ "$CXX" = "clang++" ]; then
-    CMAKE_ARGS="${CMAKE_ARGS} -DENTITYX_USE_CPP11_STDLIB=1"
-  fi
-fi
-
-cmake ${CMAKE_ARGS}
+cmake -DCMAKE_BUILD_TYPE=Debug -DENTITYX_BUILD_TESTING=1
 make VERBOSE=1
 make test || cat Testing/Temporary/LastTest.log
