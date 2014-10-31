9e4314ef1d4575caa99fee3a4ae7f479b8153a34
Alec Thomas <alec@swapoff.org>
2014-10-31 16:24:40 +1100

    Hack for VS internal error. Fixes #23 (again).

--- entityx/Event.h
+++ entityx/Event.h
@@ -159,7 +159,8 @@ class EventManager : entityx::help::NonCopyable {
    */
   template <typename E, typename ... Args>
   void emit(Args && ... args) {
-    E event(std::forward<Args>(args) ...);
+    // Using 'E event(std::forward...)' causes VS to fail with an internal error. Hack around it.
+    E event = E(std::forward<Args>(args) ...);
     auto sig = signal_for(std::size_t(E::family()));
     BaseEvent *base = &event;
     sig->emit(base);
