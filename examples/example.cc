dd64c0a692af6ee395865b2e934f4e77158c6a22
Alec Thomas <alec@swapoff.org>
2015-01-02 20:06:06 +1100

    Migrate example code to new EntityX.

--- examples/example.cc
+++ examples/example.cc
@@ -9,11 +9,10 @@
  *
  * - Separation of data via components.
  * - Separation of logic via systems.
- * - Use of events (colliding bodies trigger a CollisionEvent).
  *
  * Compile with:
  *
- *    c++ -O3 -std=c++11 -Wall -lsfml-system -lsfml-window -lsfml-graphics -lentityx example.cc -o example
+ *    c++ -O3 -std=c++11 -Wall -lsfml-system -lsfml-window -lsfml-graphics example.cc -o example
  */
 #include <cmath>
 #include <unordered_set>
@@ -25,7 +24,7 @@
 #include <iostream>
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
-#include <entityx/entityx.h>
+#include "entityx/entityx.hh"
 
 using std::cerr;
 using std::cout;
@@ -33,20 +32,12 @@ using std::endl;
 
 namespace ex = entityx;
 
-namespace std {
-template <>
-struct hash<ex::Entity> {
-  std::size_t operator()(const ex::Entity& k) const { return k.id().id(); }
-};
-}
-
-
 float r(int a, float b = 0) {
   return static_cast<float>(std::rand() % (a * 1000) + b * 1000) / 1000.0;
 }
 
 
-struct Body : ex::Component<Body> {
+struct Body {
   Body(const sf::Vector2f &position, const sf::Vector2f &direction, float rotationd = 0.0)
     : position(position), direction(direction), rotationd(rotationd) {}
 
@@ -56,40 +47,51 @@ struct Body : ex::Component<Body> {
 };
 
 
-struct Renderable : ex::Component<Renderable> {
+struct Renderable {
   explicit Renderable(std::unique_ptr<sf::Shape> shape) : shape(std::move(shape)) {}
 
   std::unique_ptr<sf::Shape> shape;
 };
 
 
-struct Fadeable : ex::Component<Fadeable> {
+struct Fadeable {
   explicit Fadeable(sf::Uint8 alpha, float duration) : alpha(alpha), d(alpha / duration) {}
 
   float alpha, d;
 };
 
 
-struct Collideable : ex::Component<Collideable> {
+struct Collideable {
   explicit Collideable(float radius) : radius(radius) {}
 
   float radius;
 };
 
+typedef ex::EntityX<ex::Components<Body, Renderable, Fadeable, Collideable>> EntityManager;
+template <typename C> using Component = EntityManager::Component<C>;
+using Entity = EntityManager::Entity;
 
-// Emitted when two entities collide.
-struct CollisionEvent : public ex::Event<CollisionEvent> {
-  CollisionEvent(ex::Entity left, ex::Entity right) : left(left), right(right) {}
 
-  ex::Entity left, right;
+namespace std {
+template <>
+struct hash<Entity> {
+  std::size_t operator()(const Entity& k) const { return k.id().id(); }
+};
+}
+
+
+struct System {
+  virtual ~System() {}
+
+  virtual void update(EntityManager &es, double dt) = 0;
 };
 
 
 // Updates a body's position and rotation.
-struct BodySystem : public ex::System<BodySystem> {
-  void update(ex::EntityManager &es, ex::EventManager &events, ex::TimeDelta dt) override {
-    Body::Handle body;
-    for (ex::Entity entity : es.entities_with_components(body)) {
+struct BodySystem : public System {
+  void update(EntityManager &es, double dt) override {
+    Component<Body> body;
+    for (Entity entity : es.entities_with_components(body)) {
       body->position += body->direction * static_cast<float>(dt);
       body->rotation += body->rotationd * dt;
     }
@@ -99,11 +101,11 @@ struct BodySystem : public ex::System<BodySystem> {
 
 // Fades out the alpha value of any Renderable and Fadeable entity. Once the
 // object has completely faded out it is destroyed.
-struct FadeOutSystem : public ex::System<FadeOutSystem> {
-  void update(ex::EntityManager &es, ex::EventManager &events, ex::TimeDelta dt) override {
-    Fadeable::Handle fade;
-    Renderable::Handle renderable;
-    for (ex::Entity entity : es.entities_with_components(fade, renderable)) {
+struct FadeOutSystem : public System {
+  void update(EntityManager &es, double dt) override {
+    Component<Fadeable> fade;
+    Component<Renderable> renderable;
+    for (Entity entity : es.entities_with_components(fade, renderable)) {
       fade->alpha -= fade->d * dt;
       if (fade->alpha <= 0) {
         entity.destroy();
@@ -118,13 +120,13 @@ struct FadeOutSystem : public ex::System<FadeOutSystem> {
 
 
 // Bounce bodies off the edge of the screen.
-class BounceSystem : public ex::System<BounceSystem> {
+class BounceSystem : public System {
 public:
   explicit BounceSystem(sf::RenderTarget &target) : size(target.getSize()) {}
 
-  void update(ex::EntityManager &es, ex::EventManager &events, ex::TimeDelta dt) override {
-    Body::Handle body;
-    for (ex::Entity entity : es.entities_with_components(body)) {
+  void update(EntityManager &es, double dt) override {
+    Component<Body> body;
+    for (Entity entity : es.entities_with_components(body)) {
       if (body->position.x + body->direction.x < 0 ||
           body->position.x + body->direction.x >= size.x)
         body->direction.x = -body->direction.x;
@@ -139,112 +141,27 @@ private:
 };
 
 
-// Determines if two Collideable bodies have collided. If they have it emits a
-// CollisionEvent. This is used by ExplosionSystem to create explosion
-// particles, but it could be used by a SoundSystem to play an explosion
-// sound, etc..
-//
-// Uses a fairly rudimentary 2D partition system, but performs reasonably well.
-class CollisionSystem : public ex::System<CollisionSystem> {
-  static const int PARTITIONS = 200;
-
-  struct Candidate {
-    sf::Vector2f position;
-    float radius;
-    ex::Entity entity;
-  };
-
-public:
-  explicit CollisionSystem(sf::RenderTarget &target) : size(target.getSize()) {
-    size.x = size.x / PARTITIONS + 1;
-    size.y = size.y / PARTITIONS + 1;
-  }
-
-  void update(ex::EntityManager &es, ex::EventManager &events, ex::TimeDelta dt) override {
-    reset();
-    collect(es);
-    collide(events);
-  };
-
-private:
-  std::vector<std::vector<Candidate>> grid;
-  sf::Vector2u size;
-
-  void reset() {
-    grid.clear();
-    grid.resize(size.x * size.y);
-  }
-
-  void collect(ex::EntityManager &entities) {
-    Body::Handle body;
-    Collideable::Handle collideable;
-    for (ex::Entity entity : entities.entities_with_components(body, collideable)) {
-      unsigned int
-          left = static_cast<int>(body->position.x - collideable->radius) / PARTITIONS,
-          top = static_cast<int>(body->position.y - collideable->radius) / PARTITIONS,
-          right = static_cast<int>(body->position.x + collideable->radius) / PARTITIONS,
-          bottom = static_cast<int>(body->position.y + collideable->radius) / PARTITIONS;
-        Candidate candidate {body->position, collideable->radius, entity};
-        unsigned int slots[4] = {
-          left + top * size.x,
-          right + top * size.x,
-          left  + bottom * size.x,
-          right + bottom * size.x,
-        };
-        grid[slots[0]].push_back(candidate);
-        if (slots[0] != slots[1]) grid[slots[1]].push_back(candidate);
-        if (slots[1] != slots[2]) grid[slots[2]].push_back(candidate);
-        if (slots[2] != slots[3]) grid[slots[3]].push_back(candidate);
-    }
-  }
-
-  void collide(ex::EventManager &events) {
-    for (const std::vector<Candidate> &candidates : grid) {
-      for (const Candidate &left : candidates) {
-        for (const Candidate &right : candidates) {
-          if (left.entity == right.entity) continue;
-          if (collided(left, right))
-            events.emit<CollisionEvent>(left.entity, right.entity);
-        }
-      }
-    }
-  }
-
-  float length(const sf::Vector2f &v) {
-    return std::sqrt(v.x * v.x + v.y * v.y);
-  }
-
-  bool collided(const Candidate &left, const Candidate &right) {
-    return length(left.position - right.position) < left.radius + right.radius;
-  }
-};
-
-
 // For any two colliding bodies, destroys the bodies and emits a bunch of bodgy explosion particles.
-class ExplosionSystem : public ex::System<ExplosionSystem>, public ex::Receiver<ExplosionSystem> {
+class ExplosionSystem : public System {
 public:
-  void configure(ex::EventManager &events) override {
-    events.subscribe<CollisionEvent>(*this);
-  }
-
-  void update(ex::EntityManager &es, ex::EventManager &events, ex::TimeDelta dt) override {
-    for (ex::Entity entity : collided) {
+  void update(EntityManager &es, double dt) override {
+    for (Entity entity : collided) {
       emit_particles(es, entity);
       entity.destroy();
     }
     collided.clear();
   }
 
-  void emit_particles(ex::EntityManager &es, ex::Entity entity) {
-    Body::Handle body = entity.component<Body>();
-    Renderable::Handle renderable = entity.component<Renderable>();
-    Collideable::Handle collideable = entity.component<Collideable>();
+  void emit_particles(EntityManager &es, Entity entity) {
+    Component<Body> body = entity.component<Body>();
+    Component<Renderable> renderable = entity.component<Renderable>();
+    Component<Collideable> collideable = entity.component<Collideable>();
     sf::Color colour = renderable->shape->getFillColor();
     colour.a = 200;
 
     float area = (M_PI * collideable->radius * collideable->radius) / 3.0;
     for (int i = 0; i < area; i++) {
-      ex::Entity particle = es.create();
+      Entity particle = es.create();
 
       float rotationd = r(720, 180);
       if (std::rand() % 2 == 0) rotationd = -rotationd;
@@ -264,20 +181,104 @@ public:
     }
   }
 
-  void receive(const CollisionEvent &collision) {
+  void on_collision(Entity left, Entity right) {
     // Events are immutable, so we can't destroy the entities here. We defer
     // the work until the update loop.
-    collided.insert(collision.left);
-    collided.insert(collision.right);
+    collided.insert(left);
+    collided.insert(right);
   }
 
 private:
-  std::unordered_set<ex::Entity> collided;
+  std::unordered_set<Entity> collided;
+};
+
+
+
+
+// Determines if two Collideable bodies have collided. If they have it emits a
+// CollisionEvent. This is used by ExplosionSystem to create explosion
+// particles, but it could be used by a SoundSystem to play an explosion
+// sound, etc..
+//
+// Uses a fairly rudimentary 2D partition system, but performs reasonably well.
+class CollisionSystem : public System {
+  static const int PARTITIONS = 200;
+
+  struct Candidate {
+    sf::Vector2f position;
+    float radius;
+    Entity entity;
+  };
+
+public:
+  explicit CollisionSystem(sf::RenderTarget &target, ExplosionSystem &explosions) : size(target.getSize()), explosions(explosions) {
+    size.x = size.x / PARTITIONS + 1;
+    size.y = size.y / PARTITIONS + 1;
+  }
+
+  void update(EntityManager &es, double dt) override {
+    reset();
+    collect(es);
+    collide();
+  };
+
+private:
+  std::vector<std::vector<Candidate>> grid;
+  sf::Vector2u size;
+  ExplosionSystem &explosions;
+
+  void reset() {
+    grid.clear();
+    grid.resize(size.x * size.y);
+  }
+
+  void collect(EntityManager &entities) {
+    Component<Body> body;
+    Component<Collideable> collideable;
+    for (Entity entity : entities.entities_with_components(body, collideable)) {
+      unsigned int
+          left = static_cast<int>(body->position.x - collideable->radius) / PARTITIONS,
+          top = static_cast<int>(body->position.y - collideable->radius) / PARTITIONS,
+          right = static_cast<int>(body->position.x + collideable->radius) / PARTITIONS,
+          bottom = static_cast<int>(body->position.y + collideable->radius) / PARTITIONS;
+        Candidate candidate {body->position, collideable->radius, entity};
+        unsigned int slots[4] = {
+          left + top * size.x,
+          right + top * size.x,
+          left  + bottom * size.x,
+          right + bottom * size.x,
+        };
+        grid[slots[0]].push_back(candidate);
+        if (slots[0] != slots[1]) grid[slots[1]].push_back(candidate);
+        if (slots[1] != slots[2]) grid[slots[2]].push_back(candidate);
+        if (slots[2] != slots[3]) grid[slots[3]].push_back(candidate);
+    }
+  }
+
+  void collide() {
+    for (const std::vector<Candidate> &candidates : grid) {
+      for (const Candidate &left : candidates) {
+        for (const Candidate &right : candidates) {
+          if (left.entity == right.entity) continue;
+          if (collided(left, right))
+            explosions.on_collision(left.entity, right.entity);
+        }
+      }
+    }
+  }
+
+  float length(const sf::Vector2f &v) {
+    return std::sqrt(v.x * v.x + v.y * v.y);
+  }
+
+  bool collided(const Candidate &left, const Candidate &right) {
+    return length(left.position - right.position) < left.radius + right.radius;
+  }
 };
 
 
 // Render all Renderable entities and draw some informational text.
-class RenderSystem  :public ex::System<RenderSystem> {
+class RenderSystem : public System {
 public:
   explicit RenderSystem(sf::RenderTarget &target, sf::Font &font) : target(target) {
     text.setFont(font);
@@ -286,10 +287,10 @@ public:
     text.setColor(sf::Color::White);
   }
 
-  void update(ex::EntityManager &es, ex::EventManager &events, ex::TimeDelta dt) override {
-    Body::Handle body;
-    Renderable::Handle renderable;
-    for (ex::Entity entity : es.entities_with_components(body, renderable)) {
+  void update(EntityManager &es, double dt) override {
+    Component<Body> body;
+    Component<Renderable> renderable;
+    for (Entity entity : es.entities_with_components(body, renderable)) {
       renderable->shape->setPosition(body->position);
       renderable->shape->setRotation(body->rotation);
       target.draw(*renderable->shape.get());
@@ -311,23 +312,23 @@ private:
 };
 
 
-class Application : public ex::EntityX {
+class Application {
 public:
   explicit Application(sf::RenderTarget &target, sf::Font &font) {
-    systems.add<BodySystem>();
-    systems.add<FadeOutSystem>();
-    systems.add<BounceSystem>(target);
-    systems.add<CollisionSystem>(target);
-    systems.add<ExplosionSystem>();
-    systems.add<RenderSystem>(target, font);
-    systems.configure();
+    systems.push_back(new BodySystem());
+    systems.push_back(new FadeOutSystem());
+    systems.push_back(new BounceSystem(target));
+    ExplosionSystem *explosions = new ExplosionSystem();
+    systems.push_back(explosions);
+    systems.push_back(new CollisionSystem(target, *explosions));
+    systems.push_back(new RenderSystem(target, font));
 
     sf::Vector2u size = target.getSize();
     for (int i = 0; i < 500; i++) {
-      ex::Entity entity = entities.create();
+      Entity entity = entities.create();
 
       // Mark as collideable (explosion particles will not be collideable).
-      Collideable::Handle collideable = entity.assign<Collideable>(r(10, 5));
+      Component<Collideable> collideable = entity.assign<Collideable>(r(10, 5));
 
       // "Physical" attributes.
       entity.assign<Body>(
@@ -342,14 +343,18 @@ public:
     }
   }
 
-  void update(ex::TimeDelta dt) {
-    systems.update<BodySystem>(dt);
-    systems.update<FadeOutSystem>(dt);
-    systems.update<BounceSystem>(dt);
-    systems.update<CollisionSystem>(dt);
-    systems.update<ExplosionSystem>(dt);
-    systems.update<RenderSystem>(dt);
+  ~Application() {
+    for (System *system : systems) delete system;
   }
+
+  void update(double dt) {
+    for (System *system : systems)
+      system->update(entities, dt);
+  }
+
+private:
+  EntityManager entities;
+  std::vector<System*> systems;
 };
 
 
