6cff504a562aed9cf0dccde0db457c853661efad
Dan Dolonius <dolonius@chalmers.se>
2017-10-09 14:11:05 +0200

    Automatic CMake detection, no FindEntityX needed

---
+++ cmake/entityxConfig.cmake
@@ -0,0 +1,4 @@
+# If we require additional dependencies, e.g. Bar 2.0.
+# include(CMakeFindDependencyMacro)
+# find_dependency(Bar 2.0 REQUIRED)
+include("${CMAKE_CURRENT_LIST_DIR}/EntityXTargets.cmake")
